9f5a91f4db877ffdbdc1559f6c28c24c512acae1
孙宇 <oncwnuA5Ebz3F15U7KIwQMAuUJTM@git.weixin.qq.com>
2020-03-02 11:26:11 +0800

    modify

--- 多页面应用程序/multi-page/src/pages/template/about.vue
+++ 多页面应用程序/multi-page/src/pages/template/about.vue
@@ -3,9 +3,8 @@
     <header>
       <h1>关于我们</h1>
     </header>
-    <button
-      class="btn"
-    >我虽然使用jQuery库注册了点击事件，但是about.html没有通过cdn引入jQuery库，about.js也没有引入jQuery库，点击我没反应，打开控制台报Uncaught ReferenceError: $ is not defined</button>
+    <h4>虽然使用jQuery库注册了点击事件，但是about.html没有通过cdn引入jQuery库，about.js也没有引入jQuery库，点击我没反应，打开控制台报Uncaught ReferenceError: $ is not defined</h4>
+    <button class="btn">我使用jQuery库注册了点击事件</button>
   </main>
 </template>
 <style lang="css" scoped>
