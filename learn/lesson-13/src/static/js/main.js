b346a1d75a1b5c505821a272eb802beff738a569
“Elements-SY <cia1068@hotmail.com>
2020-11-03 16:35:18 +0800

    modify

--- learn/lesson-13/src/static/js/main.js
+++ learn/lesson-13/src/static/js/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from '@/pages/App.vue'
-import router from '@/router'
+import router from '@/static/router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import "@/assets/font/iconfont/iconfont.css"
 import "@/static/css/common.scss"
 import "@/static/css/index.scss"
-import "../utils/filters"
+import "@/static/utils/filters"
 Vue.use(ElementUI)
 new Vue({
   router,
