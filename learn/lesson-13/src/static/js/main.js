776bbd1fc293c2da4a7c80ed5e548f1747756abc
Elements-SY <cia1068@hotmail>
2020-11-03 16:20:20 +0800

    modify

--- learn/lesson-13/src/static/js/main.js
+++ learn/lesson-13/src/static/js/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from '@/pages/App.vue'
-<<<<<<< HEAD
 import router from '@/router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -9,9 +8,6 @@ import "@/static/css/common.scss"
 import "@/static/css/index.scss"
 import "../utils/filters"
 Vue.use(ElementUI)
-=======
-import router from '@/static/router'
->>>>>>> 45fdf6ddcc9cbf9a18b40db305f553118314f34e
 new Vue({
   router,
   render: h => h(App)
