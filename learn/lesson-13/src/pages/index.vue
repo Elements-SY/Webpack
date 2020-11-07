dfa10ab80a3c05d23e5520354c1fb3072b182b9a
“Elements-SY <cia1068@hotmail.com>
2020-11-07 15:23:45 +0800

    modify

--- learn/lesson-13/src/pages/index.vue
+++ learn/lesson-13/src/pages/index.vue
@@ -24,18 +24,6 @@
             <el-button @click="getWeatherInfo">查询天气</el-button>
         </el-col>
     </el-row>
-
-    <header>
-        <h1>首页</h1>
-        <span class="iconfont icon-zhangdan"></span>
-    </header>
-    <button class="btn">
-        我通过CDN引入使用jQuery库注册了点击事件，点击我试试
-    </button>
-    <el-row>
-        <el-button v-for="(item, index) in btnArr" :type="item.type" :disabled="item.isabled" :key="index">{{ item.id | serType }}</el-button>
-    </el-row>
-    <tabs></tabs>
     <section>
         <img src="../assets/image/fashion.jpg" alt srcset />
     </section>
@@ -43,42 +31,13 @@
 </template>
 
 <script>
-import tabs from '@/components/Tabs'
 import {
     weather
 } from '@/static/http'
 export default {
-    components: {
-        tabs
-    },
+    components: {},
     data() {
         return {
-            btnArr: [{
-                id: '01',
-                name: '主要按钮',
-                type: 'primary',
-                isabled: false
-            }, {
-                id: '02',
-                name: '成功按钮',
-                type: 'success',
-                isabled: false
-            }, {
-                id: '03',
-                name: '信息按钮',
-                type: 'info',
-                isabled: false
-            }, {
-                id: '04',
-                name: '警告按钮',
-                type: 'warning',
-                isabled: false
-            }, {
-                id: '05',
-                name: '危险按钮',
-                type: 'danger',
-                isabled: true
-            }],
             weatherData: {},
             params: {
                 p: '上海市',
